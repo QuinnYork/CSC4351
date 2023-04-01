31bc6676f319d42409b177ab52c0aebcee3f5571
Quinn York <yorkquinn@gmail.com>
2023-04-01 12:39:59 -0500

    believe MipsFrame is done, have to test

--- prog4/Mips/MipsFrame.java
+++ prog4/Mips/MipsFrame.java
@@ -1,4 +1,5 @@
 package Mips;
+
 import java.util.Hashtable;
 import Symbol.Symbol;
 import Temp.Temp;
@@ -10,6 +11,8 @@ import Frame.AccessList;
 public class MipsFrame extends Frame {
 
   private int count = 0;
+  private static final int MAX_REGISTERS = 32;
+
   public Frame newFrame(Symbol name, Util.BoolList formals) {
     Label label;
     if (name == null)
@@ -21,13 +24,32 @@ public class MipsFrame extends Frame {
     return new MipsFrame(label, formals);
   }
 
-  public MipsFrame() {}
+  public MipsFrame() {
+  }
+
   private MipsFrame(Label n, Util.BoolList f) {
     name = n;
+    this.formals = getAccess(f);
+  }
+
+  private AccessList getAccess(Util.BoolList f) {
+    if (f == null)
+      return null;
+    return new AccessList(allocLocal(f.head), getAccess(f.tail));
   }
 
   private static final int wordSize = 4;
-  public int wordSize() { return wordSize; }
 
-  public Access allocLocal(boolean escape) { return null; }
+  public int wordSize() {
+    return wordSize;
+  }
+
+  public Access allocLocal(boolean escape) {
+    Temp t = new Temp();
+    if (!escape && t.key() < MAX_REGISTERS) // room in temporary storage
+      return new InReg(t);
+    count -= 4;
+    InFrame i = new InFrame(count);
+    return i; // else allocate on stack
+  }
 }
